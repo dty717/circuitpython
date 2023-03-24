663d16cecfe469dbe07c09c904facfd11e8223e7
dty717 <dty71719@gmail.com>
2023-03-24 14:14:25 +0800

    Signed-off-by: dty717 <dty71719@gmail.com>

--- main.c
+++ main.c
@@ -592,9 +592,9 @@ STATIC bool run_code_py(safe_mode_t safe_mode, bool first_run, bool *simulate_re
             printf("configuration_descriptor_template size:%d\r\n",sizeof(configuration_descriptor_template));
             // printf("device_descriptor_allocation:%s\r\n",(uint8_t *)device_descriptor_allocation->ptr);
             print_buf((uint8_t *)device_descriptor_allocation->ptr, 400);
-            printf("\r\n")
+            printf("\r\n");
             print_buf((uint8_t *)configuration_descriptor_allocation->ptr, 400);
-            printf("\r\n")
+            printf("\r\n");
             print_buf((uint8_t *)string_descriptors_allocation->ptr, 400);
 
             printed_press_any_key = true;
