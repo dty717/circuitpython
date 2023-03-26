c0909c01d2acf88b58ae00782690f255cdff596e
dty717 <dty71719@gmail.com>
2023-03-26 10:15:42 +0800

    Signed-off-by: dty717 <dty71719@gmail.com>

--- supervisor/shared/usb/usb_desc.c
+++ supervisor/shared/usb/usb_desc.c
@@ -191,12 +191,13 @@ static void usb_build_configuration_descriptor(void) {
         total_descriptor_length += usb_vendor_descriptor_length();
     }
     #endif
-    test1 = total_descriptor_length;
+    // test1 = total_descriptor_length;
 
     // Now we now how big the configuration descriptor will be, so we can allocate space for it.
     configuration_descriptor_allocation =
         allocate_memory(align32_size(total_descriptor_length),
             /*high_address*/ false, /*movable*/ false);
+    test1 = align32_size(total_descriptor_length);
     uint8_t *configuration_descriptor = (uint8_t *)configuration_descriptor_allocation->ptr;
 
     // Copy the template, which is the first part of the descriptor, and fix up its length.
